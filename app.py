e1010be7e17a80cc74e98c22e0fa2fd4dc9c7ea4
wailnefoussi77-hue <wail.nefoussi77@gmail.com>
2026-05-12 19:37:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.modele_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
